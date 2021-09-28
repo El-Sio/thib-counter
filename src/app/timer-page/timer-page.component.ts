913fbb0ada74ef1a639be55f30e881bc0483be55
Charles LESOIL <elsiokun@gmail.com>
2021-09-28 19:12:18 +0200

    code cleanup and commenting

--- src/app/timer-page/timer-page.component.ts
+++ src/app/timer-page/timer-page.component.ts
@@ -21,14 +21,13 @@ export class TimerPageComponent implements OnInit, OnDestroy {
   public timermiliHistory : number[] =[]
   public historyIndex = 0;
   public message = '';
-  public conflict_message = '';
   public hasChanged = false;
   public isReading = false;
   public timesUp = false;
   public isPlaying = false;
   public isMaxTime = false;
   public selectedChild: thibTimerValue = 
-  {"id": 0, "name":"--", "timer": 36000, "isReading": false, "isPlaying": false, "playingStart": 0, "readingStart": 0};
+  {"id": 0, "name":"--", "timer": 3600000, "isReading": false, "isPlaying": false, "playingStart": 0, "readingStart": 0};
   public childrenTime: thibTimerValue[] = [];
   public previousChild: thibTimerValue;
   public readingsub;
@@ -94,50 +93,81 @@ setGaugeValue(gauge, value) : void {
 
   ngOnInit() {
 
+    // get the current gauge graphic from the DOM
+    this.gaugeElement = document.querySelector(".gauge");
+
+    // get current child from route parameter
     this.childID = parseInt(this.route.snapshot.paramMap.get('id'));
+
+    // get children values from the server
       this.timersService.getTimerValue().subscribe(v => {
         this.childrenTime = v;
+
         v.forEach(c => {
           if(c.isPlaying || c.isReading) {
+          // if another child is reading of playing, load this child instead
             this.selectedChild = c;
             this.childID = c.id;
           }
+          // else set the current child to the ID from the route parameter.
           if(c.id === this.childID) {
             this.selectedChild = c;
           }
         });
+
+        // set component values with selected child
+
         this.previousChild = this.selectedChild;
         this.timermili = this.selectedChild.timer;
         this.isReading = this.selectedChild.isReading;
         this.isPlaying = this.selectedChild.isPlaying;
+        
+        // Display Timer values and Gauge
         this.timer = this.timeToString(this.timermili);
         this.setGaugeValue(this.gaugeElement, (this.timermili / this.MAXTIMER));
+        
+        // display success status or request.
         this.message = 'donnée reçue : ' + this.selectedChild.name + ' : ' + this.timeToString(this.timermili);
+        
+        // specific cases depending in the reveived values
+
+        // time up for this child
         if (this.timermili === 0) {this.timesUp = true}
+
+        // Max time for this child
         if (this.timermili === this.MAXTIMER) {this.isMaxTime = true;}
+
+        // If child has started reading or playing on another client start the appropriate action on this client
+        // data sync happens in the playing and reading methods.
         if(this.isPlaying) {this.startPlaying()}
         if(this.isReading) {this.startReading()}
       }, err => {
+        // handle network errors with a simple information message.
         this.message = 'erreur serveur : ' + err.message;
       });
-
-     this.gaugeElement = document.querySelector(".gauge");
-      this.isPlaying = false;
-
   }
 
 
+  // reload function (NOT USED)
   public reload(): void {
     this.redirectTo(this.location.path());
   }
 
+  // Function used to navigate to another child (each has it's own URL with ID parameter)
   public redirectTo(uri: string) {
     this.router.navigateByUrl('/', {skipLocationChange: true}).then(() =>
     this.router.navigate([decodeURIComponent(uri)]));
  }
 
+ /* This function gets fresh data from the server to compare it to local data and detect any change
+  To avoir complex conflict resolution / merge, any ongoing action (reading/playing) has priority
+  Any action that was previously recorded must be updated before applying any new change. */
+
   public hasSomethingChanged(): void {
+    // modifier detection flag
     let modif = false;
+
+    // get fresh data from the server
     this.timersService.getTimerValue().subscribe(
       v => {
         v.forEach(c => {
@@ -161,21 +191,28 @@ setGaugeValue(gauge, value) : void {
           });
         });
         if(!modif) {
+        // No Changes detected, nothing to do
         return;
         }
       }
     );
   }
 
+  // this Function handles the dropdown menu change events using 2-Way binding
   public onChange(value: any): void {
 
+    // first detect server side if something has changed and ignore menu action if it's the case
     this.hasSomethingChanged();
 
+    // special case in the menu to add a new child
     if(value === 'add_child') {
 
+      // get child name through standard navigaator input
+      // TODO : create popup component with a form
       let childname = window.prompt('Veuillez entrer le nom de l‘enfant :');
       if(childname !== null) {
       
+        // create new child locally in the table
         this.childrenTime.push({
           name:childname, 
           id:this.childrenTime[this.childrenTime.length -1].id +1,
@@ -184,68 +221,91 @@ setGaugeValue(gauge, value) : void {
           isPlaying:false,
           playingStart: 0,
           readingStart:0});
+
+          // save data
           this.saveTimer();
+
+          // refresh page to new child
           this.redirectTo('/timer/'+this.childrenTime.filter(c => c.name === childname)[0].id);
       
     } else this.redirectTo('/timer/'+this.childrenTime[this.childrenTime.length - 1].id);
+    // if empty name redirect to the last child (for UX consistency with the dropdown)
     } else {
-
+      // just load the selected child page
       this.redirectTo('/timer/'+this.selectedChild.id);
-      /*
-        this.timesUp = false;
-        this.hasChanged = false;
-        this.selectedChild = value;
-        this.timermiliHistory = [];
-        this.isReading = this.selectedChild.isReading;
-        this.isPlaying = this.selectedChild.isPlaying;
-        this.timermili = this.selectedChild.timer;
-          this.timer = this.timeToString(this.timermili);
-          this.setGaugeValue(this.gaugeElement, (this.timermili / this.MAXTIMER));
-          this.message = 'donnée reçue : ' + this.selectedChild.name + ' : ' + this.timeToString(this.timermili);
-          if (this.timermili === 0) {this.timesUp = true}
-          if (this.timermili === this.MAXTIMER) {this.isMaxTime = true;}
-          this.previousChild = this.selectedChild;
-          if(this.isPlaying) {this.startPlaying()}
-          if(this.isReading) {this.startReading()} */
     }
     }
 
+  // this function removes a child from the table and reloads data.
+  // only called from the UI with the "Supprimer" (remove) button
   public removeChild(): void {
+    
+    // Ask for user confirmation using standard navigator component.
     if (window.confirm('êtes vous sûr de vouloir supprimer le compteur de '+ this.selectedChild.name + ' ?')) {
     this.childrenTime = this.childrenTime.filter(c => c.name !== this.selectedChild.name);
+
+    // Save Data
     this.saveTimer();
+
+    // refresh page to first child (default)
     this.redirectTo('/timer/'+this.childrenTime[0].id);
   }
 
   }
 
+  // this is the reading function
+  // reading increases the available gaming time by the equivalent of reading miliseconds
   public startReading(): void {
 
+    // if reading starts on this client, start the reading process locally and inform the server
     if(this.selectedChild.readingStart === 0) {
       this.hasChanged = true;
       this.isReading = true;
       this.selectedChild.isReading = true;
+
+      // saves the timestamp of the reading session start to sync other clients
       this.selectedChild.readingStart = Date.now();
+
+      // Save Data
       this.saveTimer();
      }
 
+    // change status
     this.hasChanged = true;
+
+    // save value in history
     this.timermiliHistory.push(this.timermili);
+
+    // reading interrupts playing session
     this.selectedChild.isPlaying = false;
+
+    // keep track of the original timestamp for local display
     let originaltimer = this.timermili;
+
     this.timesUp = false;
+    
+    // use inteval observable to act each passing second
     this.readingsub = this.second$.
     subscribe( tick => {
 
+      // calculate the difference between session start and current time every second
       this.timermili = originaltimer + (Date.now() -  this.selectedChild.readingStart);
+
+      // check if time is maxed out (by config file MAX_TIMER value)
       if (this.timermili >= this.MAXTIMER) {
         this.timermili = this.MAXTIMER;
         this.isMaxTime = true;
+
+        // Stop reading action when counter is full
+        // but the child should keep reading ;-)
         this.stopReading();
       }
+
+      // display current timer value
       this.timer = this.timeToString(this.timermili);
       this.setGaugeValue(this.gaugeElement, (this.timermili / this.MAXTIMER));
 
+      // every 10 seconds check if server is still sync (in case reading started or halted on a different client)
       if (Math.round(tick/10) === tick/10) {
         
         // check if reading has stopped server side
@@ -254,8 +314,12 @@ setGaugeValue(gauge, value) : void {
 
           if(v.filter(i => i.name === this.selectedChild.name)[0].isReading === false) {
             this.timermili = v.filter(i => i.name === this.selectedChild.name)[0].timer;
+
+            // set the timer to the updated value after the reading session
             this.timer = this.timeToString(this.timermili);
             this.setGaugeValue(this.gaugeElement, (this.timermili / this.MAXTIMER));
+
+            // stop reading activity locally
             this.stopReading();
           }
 
@@ -268,16 +332,27 @@ setGaugeValue(gauge, value) : void {
 
   }
 
+  // this function stops reading action locally on the client and saves data.
   public stopReading(): void {
 
+    // unsubscribe to the interval observable.
     this.readingsub.unsubscribe();
+
+    // change status
     this.isReading = false;
     this.hasChanged = true;
     this.selectedChild.isReading = false;
+
+    // reset the reading start session timestamp
     this.selectedChild.readingStart = 0;
+
+    // save data
     this.saveTimer();
   }
 
+  // This function handles playing activity
+  // exact same implementation than Reading except that Playing DECREASES the timer value
+  // Max reading time is replaced by 0 playing time left.
   public startPlaying(): void {
 
     if(this.selectedChild.playingStart === 0) {
@@ -326,6 +401,8 @@ setGaugeValue(gauge, value) : void {
     })
   }
 
+  // This function stops Playing activity
+  // Same implementation than stopReading
   public stopPlaying(): void {
 
     this.playingsub.unsubscribe();
@@ -336,6 +413,9 @@ setGaugeValue(gauge, value) : void {
     this.saveTimer();
   }
 
+  // This function decreases timer by a value set in the config file (small)
+  // Stores previous values in history allowing for the cancel button to work
+  // does not save data (to allow for erroneous input)
   public smallPenalty(): void {
 
     this.isMaxTime = false;
@@ -350,6 +430,9 @@ setGaugeValue(gauge, value) : void {
     this.hasChanged = true;
   }
 
+  // This function increases timer by a value set in the config file (small)
+  // Stores previous values in history allowing for the cancel button to work
+  // does not save data (to allow for erroneous input)
   public smallBonus(): void {
 
     this.timesUp = false;
@@ -364,6 +447,10 @@ setGaugeValue(gauge, value) : void {
     this.hasChanged = true;
   }
 
+
+  // This function resets timer to 1 jour
+  // Stores previous values in history allowing for the cancel button to work
+  // does not save data (to allow for erroneous input)
   public resetTimer(): void {
 
     this.timesUp = false;
@@ -379,6 +466,8 @@ setGaugeValue(gauge, value) : void {
     this.hasChanged = true;
   }
 
+  // This function reverts timer value to the previously stored value using a history table
+  // TODO : check what should be the max size of this table not to crash client...
   public cancelChange(): void {
 
     this.historyIndex = this.timermiliHistory.length;
@@ -396,6 +485,9 @@ setGaugeValue(gauge, value) : void {
     }
   }
 
+  // Saves data on the server
+  // serializes the childrenTime table containing value for all children (only one has been modified)
+  // calls the API on the server with a http POST request
   public saveTimer(): void {
 
     this.childrenTime.forEach(c => {if(c.id === this.selectedChild.id) c.timer = this.timermili})
@@ -409,51 +501,7 @@ setGaugeValue(gauge, value) : void {
     });
   }
 
-  public saveTime_withCheck(): void {
-
-    let modif = false;
-    // check server side
-    this.timersService.getTimerValue().subscribe(
-      
-      v => {
-        v.forEach(c => {
-          if (c.isReading || c.isPlaying) {
-            window.alert('donnée pas à jour pour ' + c.name);
-            // something is happening for another child, reload
-            this.redirectTo('/timer/'+ c.id);
-            modif = true;
-            return;
-          }
-          this.childrenTime.forEach(e => {
-            if(e.name === c.name) {
-              if(e.timer !== c.timer) {
-                window.alert('donnée pas à jour pour ' + e.name);
-                // you are not up to date, reload
-                this.redirectTo('/timer/' + c.id);
-                modif = true;
-                return;
-              }
-            }
-          });
-        });
-        if(!modif) {
-        // save current value locally
-    this.childrenTime.forEach(c => {if(c.id === this.selectedChild.id) c.timer = this.timermili})
-        // nothing changed except local change, save
-        this.conflict_message = '';
-        let data = JSON.stringify(this.childrenTime);
-        this.timersService.setTimerValue(data).subscribe(v => {
-          this.message = 'donnée enregistrée : ' + this.selectedChild.name + ' : ' + this.timeToString(this.timermili);
-          this.hasChanged = false;
-          this.timermiliHistory = [];
-        }, err => {
-          this.message = 'erreur d‘enregistrement des données ' + err.message;
-        });
-      }
-      }
-    );
-  }
-
+  // Formatting function to transform milliseconds into a readble sentence
   public timeToString(time: number): string {
 
     let hours = Math.floor(
--- src/app/timer-service.service.ts
+++ src/app/timer-service.service.ts
@@ -10,15 +10,18 @@ import { thibTimerValue } from './data-model';
 
 export class TimerServiceService {
 
+  // get the API endpoints URL from the config file
   public timer_url = AppInitServiceService.settings.timer_url;
   public saveTimer_url = AppInitServiceService.settings.saveTimer_url;
 
   constructor(private http: HttpClient) { }
 
+  // gets a JSON array of children timer values using a simple http GET
   getTimerValue(): Observable<thibTimerValue[]> {
     return this.http.get<thibTimerValue[]>(this.timer_url);
   }
 
+  // http POST request to store a complete array of children in JSON format
   setTimerValue(timerData: string): Observable<any> {
     return this.http.post<any>(this.saveTimer_url, timerData);
   }
